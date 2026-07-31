a36a3f2b1fd89905d9b8191869f7ac66301c790e
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-07-31 23:50:55 +0530

    71A way to long words

--- Codeforces/71A.cpp
+++ Codeforces/71A.cpp
@@ -8,10 +8,10 @@ while(t--){
  string s;
  cin>>s;
  if (s.size() <= 10){
-    cout<< s ;
+    cout<< s <<endl;
  }
  else{
-    cout<<s[0]<<(s.size()-2)<<s[s.size()-1];
+    cout<<s[0]<<(s.size()-2)<<s[s.size()-1]<<endl;
  }
 }
  return 0;
